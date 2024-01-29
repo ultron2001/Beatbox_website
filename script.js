a3c310a6aa1470bf3048a028353259b625a8944f
ultron <ultronageofai@gmail.com>
2024-01-30 00:22:09 +0330

    update all design

---
+++ script.js
@@ -0,0 +1,14 @@
+var screenSize = window.innerWidth;
+
+var Allfontsize = document.body;
+
+if (screenSize == 589) {
+    Allfontsize.style.fontSize = "18px";
+} else if (screenSize == 592) {
+    Allfontsize.style.fontSize = "15px";
+} else if (screenSize == 596) {
+    Allfontsize.style.fontSize = "10px";
+}
+else {
+    Allfontsize.style.fontSize = "20px";
+}
